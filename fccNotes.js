8c1dd936460e2ae3250b54bf2b41b72e74d3ed9c
Cao Truong <caocodes@gmail.com>
2019-09-16 20:06:35 -0500

    basic JS notes
    started basic JS notes

---
+++ fccNotes.js
@@ -0,0 +1,39 @@
+//freeCodeCamp JAVASCRIPT Notes - Basic JS
+
+//single line comment
+/*multi-line comment*/
+
+/**
+ * DATA Types
+ * 
+ * 1. undefined
+ * 2. null
+ * 3. boolean
+ * 4. string
+ * 5. symbol
+ * 6. number
+ * 7. object
+ */
+
+ /**
+  *names can be alpha, numbers, $, or _
+  *no spaces, or start with numbers
+  *case sensitive
+  */
+
+  var variableName1$_; //declaration, initial value = undefined
+
+  /**
+   * assignment goes from right to left
+   */
+
+   var camelCase = 0; //declartion and initialization
+
+   var i = 0;
+   i++; //increment, i = i + 1;
+   i--; //decrement
+
+   // % remainder operator; different from modulus operator since it does not work with negative numbers
+   
+
+   
